715ed5092bbd4d434188e3b42c75848795a54693
KonstantinKhodot <Khodot.konstantin@mail.ru>
2023-02-02 13:29:39 +0300

    base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+  let arr;
+  "use strict"
+  const d = b ** 2 - 4 * a * c;
+  if(d < 0) {
+    arr = [];
+  }
   return arr;
 }
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+  let arr;
+  "use strict"
+  const d = -b / (2 * a);
+  if(d = 0) {
+    arr = [-1];
+  }
+  return arr;
+}
+function solveEquation(a, b, c) {
+  let arr;
+  "use strict"
+  const d = (-b + Math.sqrt(d) )/(2*a) && (-b - Math.sqrt(d) )/(2*a);
+  if(d > 0) {
+    arr = [-1, -4];
+  }
+  return arr;
 }
