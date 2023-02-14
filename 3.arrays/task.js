19959e7b190890bd89d709041adf0d178465a894
KonstantinKhodot <Khodot.konstantin@mail.ru>
2023-02-14 14:03:01 +0300

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+  let result = (arr1, arr2) => ((arr1.length === arr2.length) && 
+  (arr1.every((element, index) => element === arr2[index])));
+  return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
   
+}
+
+function advancedFilter(arr) {
+    let result = arr.filter(2).filter(3).map(10);
+    return result;
 }
