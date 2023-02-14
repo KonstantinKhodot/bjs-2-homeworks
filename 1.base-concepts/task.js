e6a577922e833ba5c7159cf0875b78963e1a959d
KonstantinKhodot <Khodot.konstantin@mail.ru>
2023-02-14 10:00:33 +0300

    base-concepts correction3

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,15 +1,15 @@
 function solveEquation(a, b, c) {
   let arr;
   "use strict"
-  const d = b ** 2 - 4 * a * c;
+  let d = b ** 2 - 4 * a * c;
   if(d < 0) {
     arr = [];
   }
-  const d = -b / (2 * a);
+  let d = -b / (2 * a);
   if(d = 0) {
     arr = [-1];
   }
-  const d = (-b + Math.sqrt(d) )/(2*a) && (-b - Math.sqrt(d) )/(2*a);
+  let d = (-b + Math.sqrt(d) )/(2*a) && (-b - Math.sqrt(d) )/(2*a);
   if(d > 0) {
     arr = [-1, -4];
   }
