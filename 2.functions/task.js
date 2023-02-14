27c762bc97791b0913fc8ac577f30552e85a0328
KonstantinKhodot <Khodot.konstantin@mail.ru>
2023-02-14 18:02:51 +0300

    functions correction2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -28,10 +28,8 @@ function summElementsWorker(...arr) {
         min = arr[i];
       }
       sum += arr[i];
-      console.log(summElementsWorker());
-      console.log(summElementsWorker(10, 10, 11, 20, 10));
     }
-    avg = Number((sum / arr.length).toFixed(2));
+    avg = Number(min + max);
 
   return { sum };
 }
@@ -50,10 +48,8 @@ function differenceMaxMinWorker(...arr) {
       }
       sum += arr[i];
       max = sum;
-      console.log(differenceMaxMinWorker());
-      console.log(differenceMaxMinWorker(10, 10, 11, 20, 10));
     }
-    avg = Number((sum / arr.length).toFixed(2));
+    avg = Number(max - min);
 
   return { sum };
 }
@@ -73,10 +69,8 @@ function differenceEvenOddWorker(...arr) {
       }
       sum += arr[i];
       max = sum;
-      console.log(differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17));
-      console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); 
-    }
-    avg = Number((sum / arr.length).toFixed(2));
+      }
+    avg = Number(max - min);
 
   return { sum };
 }
@@ -95,8 +89,6 @@ function averageEvenElementsWorker(...arr) {
       }
       sum += arr[i];
       max = sum;
-      console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9));
-      console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35));
     }
     avg = Number((sum / arr.length).toFixed(2));
 
@@ -104,21 +96,23 @@ function averageEvenElementsWorker(...arr) {
 }
 
 function worker(arr) {
-  let sum = 0;
-  for(let i in arr) {
-    sum += arr[i];
+  let min, max, difference;
+  min = Infinity; 
+  max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] < min)
+    min = arr[i];
+
+    if(arr[i] > max)
+    max = arr[i];
+    difference = Math.abs(max - min);
   }
-  return sum;
+  return makeWork;
 }
 
 function makeWork (...arrOfArr) {
   let max;
   arrOfArr=[[1, 2, 3, 4], [10, 20, -10, -20]];
-  const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
-  console.log(makeWork(arr, summElementsWorker));
-  console.log(makeWork(arr, differenceMaxMinWorker));
-  console.log(makeWork(arr, differenceEvenOddWorker));
-  console.log(makeWork(arr, averageEvenElementsWorker));
   for (let i in arrOfArr) {
       if (worker(arrOfArr[i]) > max) {
       max = worker(arrOfArr[i]);
