58ae2f1e5b32fc71f525f0050a98ba97610e618e
KonstantinKhodot <Khodot.konstantin@mail.ru>
2023-02-16 11:04:31 +0300

    arrays correction2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,16 @@
 function compareArrays(arr1, arr2) {
-  let result = (arr1, arr2) => ((arr1.length === arr2.length) && 
-  (arr1.every((element, index) => element === arr2[index])));
+  let result = (arr1, arr2) => (arr1.length === arr2.length) && 
+  (arr1.every(element, index)); 
+  (element === arr2[index]);
   return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
-
-function advancedFilter(arr) {
-    let result = arr.filter(2).filter(3).map(10);
-    return result;
+    let result = arr.filter(...).map(...).reduce((acc, item, index, arr)=> {
+        acc+=item;
+        if(index === arr.length - 1){
+            return acc / arr.length;
+        }
+        return acc;
+    }
 }
