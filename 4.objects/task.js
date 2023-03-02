14a4d22e45074d2cd82f76bb9e4d967b258cb15c
KonstantinKhodot <Khodot.konstantin@mail.ru>
2023-03-02 17:56:39 +0300

    objects correction2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,7 +1,7 @@
-function Student(name, gender, age) {
-    this.name = name;
+function Student(marks, gender, age) {
     this.gender = gender;
     this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
@@ -9,7 +9,7 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if ( this.marks === undefined) {
+    if (this.marks) {
         } else {
         this.marks.push(marks);
         }
@@ -21,8 +21,8 @@ Student.prototype.getAverage = function () {
     return sum / this.marks.length;  
 }
 
-Student.prototype.exclude = function (reason) {
+Student.prototype.exclude = function (excluded) {
     delete this.subject;
     delete this.marks;
-    this.reason = excluded;
+    this.excluded = excluded;
     }
