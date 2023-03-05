4c5f614f147b968a35ccff59d93ac6fb65e57ff7
KonstantinKhodot <Khodot.konstantin@mail.ru>
2023-03-05 11:41:30 +0300

    objects correction4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,12 +10,13 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if this.marks.push(marks);
+    this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
     let sum = 0;
     this.marks.forEach(item => sum += item);
+    this.marks.push(...marks);
     return 0;  
 }
 
