22befb452fce0fa295290d0a4ab0f37718c70307
KonstantinKhodot <Khodot.konstantin@mail.ru>
2023-03-06 14:38:03 +0300

    base-concepts task2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -13,3 +13,26 @@ function solveEquation(a, b, c) {
   }
   return arr;
 }
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let totalAmount;
+  "use strict"
+  Payment = S * (P + (P / (((1 + P)**n) - 1)));
+  let arr;
+  let percent = percent / 100 / 12;
+  let contribution = contribution;
+  let amount = amount;
+  if (isNaN(percent) < 0) {
+    return '"Процентная ставка"';
+    } else if (isNaN(contribution) < 0) {
+      return '"Первоначальный взнос"';
+    } else if (isNaN(amount) < 0) {
+      return '"Общая сумма"'
+    } else {
+      let today = new Date;
+      if (today.getFullYear() > Date.getFullYear()) {
+        return '"Срок ипотеки"';
+          return totalAmount;
+      }
+    }
+  }
