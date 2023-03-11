49456a0bc72f8c16b488069163a8e3b4810787f6
KonstantinKhodot <Khodot.konstantin@mail.ru>
2023-03-11 17:12:51 +0300

    arrays correction6

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -5,37 +5,39 @@ function compareArrays(arr1, arr2) {
 function getUsersNamesInAgeRange(users, gender) {
     let result = arr.filter(function(gender)), map(function(age)), reduce(function(acc, item, index, users)) => {
         acc+=item;
-        function logArray() {
-            let array = Array.from(arguments);
-            for(let element of array);
-            const arrayForPractice = new Array(30).fill(0).map(() => Math.round(Math.random() * 1000));
-        if(index === arr.length - 1){
-            return acc / arr.length;
-            const people = [
-                {firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской"},
-                {firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской"},
-                {firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский"},
-                {firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский"},
-                {firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский"},
-                {firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский"},
-                {firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской"},
-                {firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской"},
-                {firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской"},
-                {firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский"},
-                {firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской"},
-                {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
-                {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
-                {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
-              ];
-              people.filter (age);
-              people.filter (firstName);
-              people.filter (firstName.length);
-              people.filter (gender "мужской");
-              people.filter (gender "женский");
-              people.filter (gender "мужской");
-              people.filter (age "женский");
-        }
         return result;
     }
+    const newPeople = people.map(averageAge => '${averageAge}'{
+      return averageAge;
+    })
+    const people = [
+      {firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской"},
+      {firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской"},
+      {firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский"},
+      {firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский"},
+      {firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский"},
+      {firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский"},
+      {firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской"},
+      {firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской"},
+      {firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской"},
+      {firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский"},
+      {firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской"},
+      {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
+      {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
+      {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
+    ];
+    people.filter (age);
+    people.filter (firstName);
+    people.filter (firstName.length);
+    people.filter (gender "мужской");
+    people.filter (gender "женский");
+    people.filter (gender "мужской");
+    people.filter (age "женский");
+}
   }
+  function logArray() {
+    const arrayForPractice = new Array(30).fill(0).map(() => Math.round(Math.random() * 1000));
+if(index === arr.length - 1){
+    return acc / arr.length;
+}
 }
